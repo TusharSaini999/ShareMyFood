f5e4ec0fd28be85a9de3601a0cd720cd3e53ce20
TusharSaini999 <tusharsaini.in@gmail.com>
2025-02-03 22:25:21 +0530

    add a final c DB

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+const mysql=require("mysql2");
+require('dotenv').config();
+const db=mysql.createConnection({
+   host:process.env.DB_HOST,
+   password:process.env.DB_PASSWORD,
+   user:process.env.DB_USER,
+   database:process.env.DB_NAME,
+   port:process.env.dbport
 });
 
-connection.connect(err => {
-  if (err) {
-    console.error('Database connection failed:', err);
-  } else {
-    console.log('Connected to the database');
-  }
+db.connect((err) => {
+ if (err) {
+   console.error('Error connecting to the database:', err.stack);
+   return;
+ }
+ console.log('Connected to the database');
 });
-
-module.exports = connection;
+module.exports = db;
