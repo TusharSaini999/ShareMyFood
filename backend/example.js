63b096cea54d38d8778ef1982cb793ae01dc01a8
TusharSaini999 <tusharsaini.in@gmail.com>
2025-02-03 22:36:22 +0530

    test connect

--- backend/example.js
+++ backend/example.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Example GET API
+//curl -X GET http://localhost:4000/example -H "x-api-key: hjsBDhjcbdm$%ws42alfndm465a545eafdnsdkjnsd"
 router.get('/', (req, res) => {
   res.json({ message: "Welcome to the Example API" });
 });
